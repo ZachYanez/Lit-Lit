03714f9d864dcb3f43a628fcaedf33f848f91b0d
Zach Yanez <zachyanez@gmail.com>
2021-01-12 10:07:02 -0600

    adding api request

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import AppBar from '../Components/AppBar'
+import API from '../utils/api'
+
 
 export default function Home() {
+
+     useEffect(() =>{
+          API
+            .getVolume()
+            .then(res => {
+                this.state({shelf: res.data})
+                console.log(res.data.results)
+            })
+            .catch(err => console.log(err))
+          });
+
   return (
     <div>
       <AppBar />
